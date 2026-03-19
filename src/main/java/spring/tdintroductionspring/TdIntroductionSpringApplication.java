517f617fc5957b24b0ed5ec84b2ab4637a9d8a50
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-03-19 17:17:40 +0300

    feat: initialize project with core dependencies

---
+++ src/main/java/spring/tdintroductionspring/TdIntroductionSpringApplication.java
@@ -0,0 +1,13 @@
+package spring.tdintroductionspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdIntroductionSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdIntroductionSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/tdintroductionspring/TdIntroductionSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.tdintroductionspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdIntroductionSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
