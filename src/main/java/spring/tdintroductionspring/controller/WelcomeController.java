16ce1710c23db3430a00c4981ff2d926f978bed0
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-03-19 18:22:24 +0300

    feat: add GET /welcome endpoint in controller with RequestParam

---
+++ src/main/java/spring/tdintroductionspring/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package spring.tdintroductionspring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome(@RequestParam String name){
+        return "Welcome "+name;
+    }
+}
