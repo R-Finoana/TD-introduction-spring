27b80e2c0439fef20f514808ecbab0b790835774
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-03-19 19:35:18 +0300

    feat: add Http status for GET /welcome

--- src/main/java/spring/tdintroductionspring/controller/WelcomeController.java
+++ src/main/java/spring/tdintroductionspring/controller/WelcomeController.java
@@ -1,5 +1,7 @@
 package spring.tdintroductionspring.controller;
 
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
 
     @GetMapping("/welcome")
-    public String welcome(@RequestParam String name){
-        return "Welcome "+name;
+    public ResponseEntity<String> welcome(@RequestParam(required = false) String name){
+        if(name == null || name.trim().isEmpty()){
+            return ResponseEntity.badRequest().body("Name is required");
+        }
+        return ResponseEntity.ok("Welcome "+name);
     }
 }
